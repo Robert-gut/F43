0353fab158b3785f8224458937a5c4219715f0ca
Robert <Robert@step.rivne>
2025-02-12 20:27:26 +0200

    object 2

--- JS/11 JS (Object)/js/main.js
+++ JS/11 JS (Object)/js/main.js
@@ -197,25 +197,267 @@ const arrPerson = ['Robert','Barnabishvili','gmail.com',['Fron-end', 'Back-end',
 
 //! this
 
-const car = {
-    brand: 'Nissan',
-    model: 'X-Trail',
-    age: 14,
-    engin: 2.0,
-    enginType: 'disel',
-    color: 'broun',
-    carBody: 'jeep',
-    plusAge: function(newAge) {
-        if(newAge > this.age){
-            this.age = newAge
-        } else {
-            console.log('Error');
-        }
-    }
-}
+// const car = {
+//     brand: 'Nissan',
+//     model: 'X-Trail',
+//     age: 14,
+//     engin: 2.0,
+//     enginType: 'disel',
+//     color: 'broun',
+//     carBody: 'jeep',
+//     plusAge: function(newAge) {
+//         if(newAge > this.age){
+//             this.age = newAge
+//         } else {
+//             console.log('Error');
+//         }
+//     }
+// }
 
 
-car.plusAge(15)
-car.plusAge(13)
+// car.plusAge(15)
+// car.plusAge(13)
 
-console.log('✌️car --->', car);
+// console.log('✌️car --->', car);
+
+
+// const car = {
+//     brand: 'Nissan',
+//     model: 'X-Trail',
+//     age: 14,
+//     engin: 2.0,
+//     enginType: 'disel',
+//     color: 'broun',
+//     carBody: 'jeep',
+//     plusAge: function(newAge) {
+//         if(newAge > this.age){
+//             this.age = newAge
+//         } else {
+//             console.log('Error');
+//         }
+//     }
+// }
+
+
+// function count(obj) {
+//     let suma = 0
+//     for (const key in obj) {
+//         suma++
+//     }
+//     return suma
+// }
+// console.log(count(car));
+
+// Object.keys(car).length
+
+
+
+// const arr = [
+//     {price: 1},
+//     {price: 3},
+//     {price: 2},
+//     {price: 4},
+//     {price: 5},
+// ]
+
+
+// function sortPrice(arr, method) {
+//     if(method === 'top'){
+//         return arr.sort((a,b)=> a.price - b.price)
+//     } else if(method === 'bot'){
+//         return arr.sort((a,b)=> b.price - a.price)
+//     }
+// }
+
+// console.log(sortPrice(arr, 'bot'));
+// console.log(sortPrice(arr, 'top'));
+
+
+// const object = {
+//     name1: 1,
+//     name2: 2,
+//     name3: 3,
+//     name4: 4,
+// }
+
+// for (const key in object) {
+//     console.log(key);
+//     console.log(object[key]);
+// }
+
+// fetch('https://jsonplaceholder.typicode.com/comments')
+//       .then(response => response.json())
+//       .then(json => json.forEach(post => {
+//         console.log(
+//             `
+//             Email:${post.email}
+//             title:${post.name}
+//             Body:${post.body}
+//             `
+//           )
+//       }))
+
+// fetch('https://jsonplaceholder.typicode.com/comments')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+
+
+
+// const car = {
+//     brand: 'Nissan',
+//     model: 'X-Trail',
+//     age: 14,
+//     engin: 2.0,
+//     enginType: 'disel',
+//     color: 'broun',
+//     carBody: 'jeep',
+//     radius: {r:18, radius: [17,18,19]},
+//     plusAge: function(newAge) {
+//         if(newAge > this.age){
+//             this.age = newAge
+//         } else {
+//             console.log('Error');
+//         }
+//     }
+// }
+
+//! 1 не глибоке копіювання
+// const copy = Object.assign({}, car)
+// copy.engin = 3
+// copy.radius.r = 19
+
+// console.log('✌️car --->', car);
+// console.log('✌️copy --->', copy);
+
+
+
+//! 2 не глибоке копіювання
+// const newCopy = {...car}
+
+// newCopy.engin = 3
+// newCopy.radius.r = 19
+
+// console.log('✌️car --->', car);
+// console.log('✌️copy --->', newCopy);
+
+
+//! ГЛИБОКЕ КОПІЮВАННЯ
+
+// const car = {
+//     brand: 'Nissan',
+//     model: 'X-Trail',
+//     age: 14,
+//     engin: 2.0,
+//     enginType: 'disel',
+//     color: 'broun',
+//     carBody: 'jeep',
+//     radius: {r:18, radius: [17,18,19]},
+//     plusAge: function(newAge) {
+//         if(newAge > this.age){
+//             this.age = newAge
+//         } else {
+//             console.log('Error');
+//         }
+//     }
+// }
+
+
+
+// const carCopy = JSON.parse(JSON.stringify(car))
+
+
+// carCopy.engin = 3
+// carCopy.radius.r = 19
+
+// console.log('✌️car --->', car);
+// console.log('carObj --->', carCopy);
+
+
+// const deta = {
+//     at: new Date()
+// }
+
+
+// const copy1 = {...deta}
+
+// const copy2 = JSON.parse(JSON.stringify(deta))
+
+
+
+// console.log('✌️deta --->', deta.at.getFullYear());
+// console.log('✌️copy1 --->', copy1.at.getFullYear());
+// console.log('copy2 --->', copy2.at.getFullYear());
+
+
+////////////////////////////////////////////////////////
+
+
+// const car = {
+//     brand: 'Nissan',
+//     model: 'X-Trail',
+//     age: 14,
+//     engin: 2.0,
+//     enginType: 'disel',
+//     color: 'broun',
+//     carBody: 'jeep',
+//     radius: {r:18, radius: [17,18,19]},
+//     at: new Date(),
+//     test: () => {return 1}
+// }
+
+
+// const carCopy = structuredClone(car)
+
+// carCopy.engin = 3
+// carCopy.radius.r = 19
+
+// console.log('✌️car --->', car);
+// console.log('✌️carCopy --->', carCopy);
+// console.log('✌️carCopy --->', carCopy.at.getFullYear());
+
+
+//! методи object
+
+// const car = {
+//     brand: 'Nissan',
+//     model: 'X-Trail',
+//     age: 14,
+//     engin: 2.0,
+//     enginType: 'disel',
+//     color: 'broun',
+//     carBody: 'jeep',
+//     radius: {r:18, radius: [17,18,19]},
+//     at: new Date(),
+//     test: () => {return 1}
+// }
+// console.log('✌️car --->', car);
+
+// //! Object.keys() => [kyes]
+
+// const keys = Object.keys(car)
+// console.log('✌️keys --->', keys);
+
+// //! Object.values() => [values]
+
+// const values = Object.values(car)
+// console.log('✌️values --->', values);
+
+// //! Object.entries() => [[key,value]]
+
+// const entries = Object.entries(car)
+// entries.forEach(el => console.log(el[0] + ':' + el[1]))
+// console.log('✌️entries --->', entries);
+
+// //! Object.defineProperty()
+
+// const test = Object.defineProperty(car, 'clearance', {
+//     value: 250,
+//     enumerable: true
+// })
+
+// console.log('✌️test --->', test);
+
+
+// console.log(Object.keys(car).length)
+
